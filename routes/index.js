16b36870594f18d2af183b6c3f2c10e72bce1785
unknown <greyguard89@mail.ru>
2023-05-29 22:16:52 +0300

    sprint 13 create server it1 add all addres

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
-const process = require('process');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-router.use('/*', () => {
-  process.on('uncaughtException', (err, origin) => {
-    console.log(`${origin} ${err.name} c текстом ${err.message} не была обработана. Обратите внимание!`);
+router.use((req, res) => {
+  res.status(404).send({
+    message: 'Такой страницы не существует',
   });
 });
 
