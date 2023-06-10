89196ad1b5438fb45e0c6e9c133fb107ddbcff5f
unknown <greyguard89@mail.ru>
2023-06-10 21:59:29 +0300

    14 sprt add autorization + validation_12 1it

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -10,7 +10,7 @@ const validateUser = celebrate({
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    _id: Joi.string().guid().length(24),
+    _id: Joi.string().hex().length(24),
   }),
 });
 
@@ -20,7 +20,7 @@ const validateCard = celebrate({
     link: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
       // eslint-disable-next-line no-useless-escape
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
-    _id: Joi.string().guid().length(24),
+    _id: Joi.string().hex().length(24),
   }),
 });
 
