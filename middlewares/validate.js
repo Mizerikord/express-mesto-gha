b1996a353ddd239d81d3a13f6c0d21dacf7c7a14
unknown <greyguard89@mail.ru>
2023-06-10 21:45:39 +0300

    14 sprt add autorization + validation_11 1it

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -10,19 +10,17 @@ const validateUser = celebrate({
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    _id: Joi.string().length(24),
+    _id: Joi.string().guid().length(24),
   }),
 });
 
 const validateCard = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required()
-      .default('Cardname'),
-    link: Joi.string().required()
-      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
+    name: Joi.string().min(2).max(30).default('Cardname'),
+    link: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
       // eslint-disable-next-line no-useless-escape
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
-    _id: Joi.string().length(24),
+    _id: Joi.string().guid().length(24),
   }),
 });
 
