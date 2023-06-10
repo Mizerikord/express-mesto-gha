f57110eafc8ef95271fe2b2a789b56bfe498e81b
unknown <greyguard89@mail.ru>
2023-06-10 22:33:46 +0300

    14 sprt add autorization + validation_17 1it

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -4,7 +4,7 @@ const validateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
-    avatar: Joi.string().required()
+    avatar: Joi.string()
       .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
       // eslint-disable-next-line no-useless-escape
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
