6c4f6b2e13a950d2edfdfedb5cae44c2d361962d
unknown <greyguard89@mail.ru>
2023-06-10 22:06:45 +0300

    14 sprt add autorization + validation_13 1it

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -10,8 +10,8 @@ const validateUser = celebrate({
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
-    _id: Joi.string().hex().length(24),
   }),
+  _id: Joi.string().hex().length(24),
 });
 
 const validateCard = celebrate({
@@ -20,8 +20,8 @@ const validateCard = celebrate({
     link: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png')
       // eslint-disable-next-line no-useless-escape
       .regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
-    _id: Joi.string().hex().length(24),
   }),
+  _id: Joi.string().hex().length(24),
 });
 
 const validateAvatar = celebrate({
